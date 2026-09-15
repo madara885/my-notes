0ea4d50d43d8e1c1d0f8d7bc572f9b32f0799217
madara885 <artem54moroz@gmail.com>
2026-09-15 10:19:17 +0300

    Добавлена папка mynotes

---
+++ mynotes/my-node-app/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js in Docker! 🐳');
+});
+
+app.listen(port, () => {
+  console.log(`Сервер успешно запущен внутри Docker на порту ${port}`);
+});
---
+++ mynotes/rust-docker/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    eprintln!("Hello from Rust inside Docker! 🦀");
+    // небольшая задержка для гарантии вывода
+    std::thread::sleep(std::time::Duration::from_millis(100));
+}
